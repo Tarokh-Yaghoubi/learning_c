4bb85e2844e97b21317d4217a8275634f775b125
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-03-26 00:12:21 +0330

    a brief explanation on function return types and parameters

---
+++ 0000_objects_functions_types/0001_function_types.c
@@ -0,0 +1,15 @@
+/**
+*
+*   Author: Tarokh Yaghoubi
+*   Description: 
+*       - Function Types in the C programming language
+*       - How to declare and use functions in C
+*/
+
+// NOTES:
+// - Function  types are derived types. in this case, type is "derived" from the return type and the number and types of its parameters.
+// - The return type of a function cannot be an array type.
+// - When you declare a function, you use the "function declarator" to specify the name of the function and the return type.
+// - If the function declarator includes a parameter type list and a definition, the declaration of each parameter must include an "identifier"
+// except parameter lists with only a single parameter of type "void", which needs no identifier.
+
