30b7b5d0cacdee5733a7b344e986b0872a8e3fea
Tarokh Yaghoubi <tarokhgit@gmail.com>
2024-04-12 22:40:00 +0330

    socket server | Initial Commit

---
+++ sockets/server.c
@@ -0,0 +1,26 @@
+/**
+ * @file server.com
+ * @author Tarokh Yaghoubi
+ * @brief  Socket-Server examples 
+ * @version 0.1
+ * @date 2024-04-12
+ * 
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <unistd.h>
+ #include <netinet/in.h>
+ #include <sys/socket.h>
+
+ #define PORT 5555
+
+ int
+ main(int argc, char* argv[]) {
+
+    // Socket Server
+
+
+ }
+ 
