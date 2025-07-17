622ed2a81c738b0b3d41e38bb2988d040a8dc625
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-07-17 16:14:46 +0330

    Floats in C, how to write a float and print it

---
+++ types_II/floats.c
@@ -0,0 +1,18 @@
+
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+	
+	float f = 3.14159;
+	float g = 0.00000000265f;
+
+	printf("%.5f\n", f);
+	printf("%.11f\n", g);
+
+	return 0;
+
+}
