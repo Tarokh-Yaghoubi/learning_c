d12aa344035ccd8d0f6e8ca6d37d8522555ffd86
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-07-20 19:51:31 +0330

    Command-Line Arguments - C programming

---
+++ OutsideEnvironment/argc_argv.c
@@ -0,0 +1,29 @@
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// After the last string, argv is a pointer to NULL
+// argv[argc] == NULL is always true
+
+// char *argv[] == char **argv
+// These are equivalent, you can use them both
+
+
+int
+main(int argc, char **argv) {
+
+	int total = 0;
+	int value = 0;
+	for (int i = 1; i < argc; i++) {
+		value = atoi(*(argv + i));
+		total += value;
+	}
+
+	printf("TOTAL -> %d\n", total);
+
+	return EXIT_SUCCESS;
+}
+
+
