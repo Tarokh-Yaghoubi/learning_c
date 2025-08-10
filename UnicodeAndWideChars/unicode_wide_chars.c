bbbfdacd4b54b3f4d684e4b7c02de3ace400672d
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-08-10 20:52:52 +0330

    Basic Notes on Unicode and Code points -  C programming

--- UnicodeAndWideChars/unicode_wide_chars.c
+++ UnicodeAndWideChars/unicode_wide_chars.c
@@ -1,1 +1,28 @@
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+ *	It's about two things:
+ *		First we have the code point which tells us effectively the serial number of
+ *		a particular character.
+ *
+ * 		Second, we have the encoding which tells us how we are going to represent that number in memory.
+ *	
+ *		With UTF-16 and UTF-32 byte-order matters, so we see UTF_16BE for big-endian, and UTF-16LE
+ *		technically if unspecified, you should assume big-endian. 
+ *		Example: 
+ *
+ *		Char    Code Point   UTF-16BE   UTF32-BE   UTF16-LE    UTF-32LE    UTF-8
+ *		π(PI)        3C0        03C0     000003C0     C003      C0030000    CF80
+ * */
+
+
+
+int main() {
+
+
+
+
+}
