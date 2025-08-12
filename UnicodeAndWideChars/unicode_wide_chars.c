3d4750a2aa1beb309771e9f672895fb6b0a22827
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-08-12 09:02:29 +0330

    mbstowcs() and strlen in C

--- UnicodeAndWideChars/unicode_wide_chars.c
+++ UnicodeAndWideChars/unicode_wide_chars.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include <wchar.h>
+#include <locale.h>
+
 /*
  *	It's about two things:
  *		First we have the code point which tells us effectively the serial number of
@@ -24,6 +27,7 @@
 
 int main() {
 
+
 	char* str = "\u20AC 1.23";
 	printf("EU Price -> %s\n", str);
 
@@ -34,6 +38,26 @@ int main() {
 	
 	// These two are the same \u03C0 == \U000003C0
 	
+	const char* utf8_japanese = "\U00003041\U00003052\U00003073\U00003064\U00003081";
+	
+	// This is supposed to be 5 but it is 15, cause strlen is counting the bytes and it will mislead you
+	// with other character sets, such as Japanese or chinese
+
+	printf("uf8_japanese => [%zu]\n", strlen(utf8_japanese));
+
+	// mbstowcs() => Converts a multibyte string (char*). into a wide-char string wchar_t* and returns the number of wide chars 
+	// stored - excluding the null terminator.
+	
+	// size_t mbstowcs(wchar_t *dest, const char* src, size_t n);
+	
+	setlocale(LC_ALL, ""); // enable UTF8 Handling
+	
+	wchar_t wbuf[20];
+
+	size_t len = mbstowcs(wbuf, utf8_japanese, 20);
+	size_t len2 = mbtowcs
+	printf("mbstowcs len => [%zu]\n", len);
+
 	printf("\u0024\n");
 
 	return 0;
