e9eb447f16392c9314b63178a4cfdb7fc4e47d89
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-08-13 16:40:17 +0330

    Initial Commit

---
+++ UnicodeAndWideChars/wchar_t_examples.c
@@ -0,0 +1,18 @@
+	// mbtowc => multibyte character to wide character
+	// wctomb => wide character to multi byte
+	// mbstowcs => multibyte string to wide character string
+	// wcstombs => wide character string to multibyte string
+	
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <wchar.h>
+#include <string.h>
+#include <locale.h>
+#include <wchar.h>
+
+int main(void) {
+
+
+	return EXIT_SUCCESS;
+}
