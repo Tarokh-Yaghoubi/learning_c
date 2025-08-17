76b406c9ac37058e1db12958f70611e16991988a
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-08-17 15:57:49 +0330

    SigHandler Examples - C programming

--- SignalHandling/sighandler.c
+++ SignalHandling/sighandler.c
@@ -10,14 +10,20 @@
 int count = 0;
 
 void sigint_handler(int signum) {
-/*
+
+	// This is a portable code which also gets along with the ancient UNIX behaviours.
+	// In ancient UNIX behaviour, once a signal was delivered, the handler would reset to the default action 
+	// which, for SIGINT is to terminate the program.
+	
+	// That means the handler would only run once.
+
 	signal(SIGINT, sigint_handler);
 
 	(void)signum;	 // get rid of unused variable warning
 	
-*/
-
+	
 	count++;
+
 	printf("Count : %d\n", count);
 
 	if (count == 2) {
