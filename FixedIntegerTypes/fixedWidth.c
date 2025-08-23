809e7b8d78316ff86820f7504e8ed873857b6206
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-08-23 11:24:11 +0330

    How revert works in bitwise - two's complement

--- FixedIntegerTypes/fixedWidth.c
+++ FixedIntegerTypes/fixedWidth.c
@@ -1,5 +1,6 @@
 
 
+
 #include "../requirements.h"
 
 int main() {
@@ -14,8 +15,24 @@ int main() {
 	// Integers of exactly a ceratin size -> intN_t
 	// Integers that are at least a certain size (int_leastN_t)
 	// Integers that are at least a certain size and are as fast as possible (int_fastN_t)
-	
+	int32_t w;	// w is exactly 32 bits, signed 
+	uint16_t x;	// x is exactly 16 bits, unsigned
+	int_least8_t y;	// y is at least 8 bits, signed 
 
+	uint_fast64_t z; // z is the fastest representation at least 64 bits, unsigned.
+
+	// There's a type you can use that holds the largest representable integers available 
+	// on the system, both signed and unsigned :
+	// intmax_t and uintmax_t
+	intmax_t maxInt;
+	uintmax_t maxUint;
+	int first = 34;
+	int second = 34;
+	printf("%d\t%d\n", ~first, second);
+	// If you have a constant that you want to have fit in a certain number of bits,
+	// you cna use these macros to automatically append the proper suffix onto the number
+	// like INT8_C(x), UINT16_C(x), INTMAX_C(x);
 
 	return EXIT_SUCCESS;
 }
+
