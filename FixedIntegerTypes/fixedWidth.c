055cc479543ff224ec0a82b4b347609e60edb038
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-08-23 10:53:38 +0330

    Fixed Signed/Unsigned Int types in C - C programming

---
+++ FixedIntegerTypes/fixedWidth.c
@@ -0,0 +1,21 @@
+
+
+#include "../requirements.h"
+
+int main() {
+
+	// The bit-sized types
+	
+	// for both signed and unsigned integers, we can specify a type that is a certain 
+	// number of bits, with some caveats.
+
+	printf("%ld\n", INT_MAX);
+
+	// Integers of exactly a ceratin size -> intN_t
+	// Integers that are at least a certain size (int_leastN_t)
+	// Integers that are at least a certain size and are as fast as possible (int_fastN_t)
+	
+
+
+	return EXIT_SUCCESS;
+}
