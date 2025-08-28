12de1afd62e21065f7feb5bb5af96ea8dfb000f5
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-08-28 17:21:50 +0330

    Threads - Playing with thrd_join

--- Multithreading/posix_threads.c
+++ Multithreading/posix_threads.c
@@ -1,6 +1,6 @@
 
 #include "../requirements.h"
-
+#include <threads.h>
 
 // C11, introduced multithreading to the C language.
 // It is very similar to POSIX threads.
@@ -19,8 +19,49 @@
 // If you get linker errors, that could be because the appropriate library is not imported
 
 
-int main() {
+#define THREAD_COUNT 5
 
+int run(void * arg) {
+
+	int a = *(int*)arg;
+
+	printf("THREAD: running thread with arg -> %d\n", a);
+
+	return a;
+
+}
+
+
+int main(void) {
+
+	// we have thread local storage, mutexes, conditional variables
+	// thrd_t is a unique identifer per thread
+	// if you do not thrd_join after the thread for the sake of cleanup
+	// you'll leak memory,
+	// unless you call thrd_detach()
+
+	thrd_t t[THREAD_COUNT];
+	int i;
+
+	printf("Launching threads... \n");
+
+	for (i = 0; i < THREAD_COUNT; i++)
+		thrd_create(t + i, run, &i);
+
+
+
+	printf("Doing other things while the thread runs... \n");
+	printf("Waiting threads to complete ... \n");
+
+	for (int i = 0; i < THREAD_COUNT; i++) {
+		int res;
+		thrd_join(t[i], &res);
+
+		printf("THREAD %d completed...\n", res);
+	}
+
+	printf("All threads completed...\n");
 
 	return EXIT_SUCCESS;
 }
+
