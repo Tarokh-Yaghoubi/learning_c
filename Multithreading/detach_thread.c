eec253ae70a91b3cac92b5347a8a6348c6c172b7
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2025-08-29 12:54:40 +0330

    Some more notes on thrd_detach() and also thrd_current()

--- Multithreading/detach_thread.c
+++ Multithreading/detach_thread.c
@@ -8,18 +8,26 @@
 int run(void* arg) {
 	(void)arg;
 	
+	printf("Thread Running ID -> %lu\n", thrd_current()); // this is not portable
+	// because the spec does not say what is the type of a thrd_t under the hood.
+	// it could even be a struct... not an unsigned int 
+	
 	printf("Thread Running -> \n");
 
 	return 0;
 }
 
+// Some threads may have duplicate ID's in this way
+// C is allowed to reuse thread ID's after the corresponding thread has exited 
+// So some threads may finish their run before others are lunched
 
 int main(void) {
 	thrd_t t;
 	
 	for (int i = 0; i < THREAD_MAX; i++) {
 		thrd_create(&t, run, NULL);
-		thrd_detach(t);
+		thrd_detach(t);	// cause we are detaching we do not need another thrd_t for the next thread creation
+		// we just overwrite it.
 	}
 	
 	thrd_sleep(&(struct timespec){.tv_sec=1}, NULL);
